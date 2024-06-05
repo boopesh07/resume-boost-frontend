8786c2202ae11bccf30a61dbe45f55599f690cba
boopesh07 <51117963+boopesh07@users.noreply.github.com>
2024-06-04 18:54:54 -0700

    Backend and Front Integration is done.

--- src/app/cover/page.tsx
+++ src/app/cover/page.tsx
@@ -1,53 +1,54 @@
-// pages/cover.tsx
 "use client"; // Ensure this component is treated as a client component
 
-import React, { useState } from 'react';
-import CoverLetter from '../../components/CoverLetter';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import CoverLetter from '@/components/CoverLetter';
 
-const mockData = {
-  cover_letter: `Boopesh Shanmugam  
-San Francisco, CA 94132  
-+1 6284682302  
-s.boopesh@gmail.com  
-[LinkedIn Profile](https://www.linkedin.com/in/boopesh-shanmugam/)
-
-[Date]
-
-Hiring Manager  
-The Aspen Group (TAG)  
-Chicago, IL  
-
-Dear Hiring Manager,
-
-I am writing to express my interest in the Software Developer - Front End position at The Aspen Group (TAG). With over three years of hands-on experience in software development at Harness, I am confident in my ability to contribute effectively to your team’s ongoing success and help modernize TAG's tech stack. I am particularly excited about the opportunity to work with NextJS/React, headless CMS, and modern CI/CD pipelines to enhance the consumer healthcare experiences provided by your brands.
-
-At Harness, I spearheaded the architectural design and development of microservices using technologies such as Spring Boot Java, Redis for messaging, and Docker for containerization, streamlining deployments through CI/CD workflows. This experience is directly aligned with TAG's emphasis on ensuring quality through automation tests such as Cypress and Playwright. My robust background in developing RESTful APIs and working with both Redis and GraphQL services will allow me to seamlessly integrate and contribute to your efforts in moving to a common platform.
-
-Key achievements from my tenure at Harness include:
-
-- Developing a Favorites Framework, significantly reducing user navigation efforts by 80%, enhancing the user experience, and contributing to customer retention.
-- Engineering sophisticated integrations with Google Cloud Build, leveraging Kubernetes for optimal performance and achieving substantial deal value.
-- Leading a comprehensive security overhaul, addressing over 30+ vulnerable libraries and thereby reinforcing product security and SOC2 compliance.
-
-Furthermore, my project experience, such as solo developing a full stack application to identify performance bottlenecks using React and integrating monitoring tools, has honed my ability to translate designs and wireframes into high-quality code, optimize component performance, and submit thorough peer code reviews.
-
-In pursuing my MBA with a focus on Business Analytics, I have further refined my capacity to contribute to data-driven decision-making processes, which aligns well with TAG's focus on A/B testing and rapid experimentation.
-
-I am highly enthusiastic about bringing my skill set to TAG, a leader in retail healthcare, and contributing to your mission of making healthcare smarter and better for everyone. I am eager to be part of a team that leverages cutting-edge technology to provide high-quality consumer healthcare experiences at scale.
-
-Thank you for considering my application. I look forward to the opportunity to discuss how my background, skills, and aspirations align with the needs of your development team.
-
-Sincerely,  
-Boopesh Shanmugam`
-};
+interface CoverLetterData {
+  cover_letter: string;
+}
 
 const CoverPage: React.FC = () => {
-  const [parsedData, setParsedData] = useState(mockData);
+  const router = useRouter();
+  const [parsedData, setParsedData] = useState<CoverLetterData | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const query = new URLSearchParams(window.location.search);
+    const runId = query.get('runId');
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/view_cover/${runId}`);
+        if (response.ok) {
+          const data: CoverLetterData = await response.json();
+          setParsedData(data);
+        } else {
+          setError('Failed to fetch cover letter data');
+        }
+      } catch (error) {
+        setError('An error occurred');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (runId) {
+      fetchData();
+    } else {
+      setLoading(false);
+      setError('Run ID not provided');
+    }
+  }, []);
+
+  if (loading) return <div className="loader">Loading...</div>;
+  if (error) return <div className="error">{error}</div>;
 
   return (
     <div className="min-h-screen bg-gray-900 text-white p-8">
       <h1 className="text-3xl font-bold mb-8 text-center">Generated Cover Letter</h1>
-      <CoverLetter content={parsedData.cover_letter} />
+      {parsedData && <CoverLetter content={parsedData.cover_letter} />}
     </div>
   );
 };
--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,4 +1,3 @@
-// components/InputForm.tsx
 "use client"; // This directive will ensure the component is treated as a client component
 
 import React, { useState } from 'react';
@@ -8,6 +7,7 @@ const InputForm: React.FC = () => {
   const [jobDescription, setJobDescription] = useState('');
   const [resume, setResume] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   const validateInputs = () => {
@@ -31,6 +31,7 @@ const InputForm: React.FC = () => {
     e.preventDefault();
     if (!validateInputs()) return;
 
+    setLoading(true);
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/generate_resume`, {
       method: 'POST',
       headers: {
@@ -38,9 +39,11 @@ const InputForm: React.FC = () => {
       },
       body: JSON.stringify({ job_description: jobDescription, resume_text: resume }),
     });
+    setLoading(false);
+
     if (response.ok) {
       const data = await response.json();
-      router.push(`/resume?data=${encodeURIComponent(JSON.stringify(data))}`);
+      router.push(`/resume?runId=${data.run_id}`);
     } else {
       console.error('Failed to boost resume');
     }
@@ -50,6 +53,7 @@ const InputForm: React.FC = () => {
     e.preventDefault();
     if (!validateInputs()) return;
 
+    setLoading(true);
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/generate_cover_letter`, {
       method: 'POST',
       headers: {
@@ -57,9 +61,11 @@ const InputForm: React.FC = () => {
       },
       body: JSON.stringify({ job_description: jobDescription, resume_text: resume }),
     });
+    setLoading(false);
+
     if (response.ok) {
       const data = await response.json();
-      router.push(`/cover?data=${encodeURIComponent(JSON.stringify(data))}`);
+      router.push(`/cover?runId=${data.run_id}`);
     } else {
       console.error('Failed to generate cover letter');
     }
@@ -67,67 +73,73 @@ const InputForm: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-900 text-white p-4">
-    <h1 className="text-3xl font-bold mt-10 mb-5 tracking-wider">
-      Welcome to Resume Boost
-    </h1>
-    <p className="mb-5 font-medium tracking-wider">
-      Please upload your resume and the job description below
-    </p>
-    <form className="w-full max-w-7xl" onSubmit={handleBoostResume}>
-      <div className="flex  justify-between gap-20 mb-8">
-        <div className="">
-          <label
-            className="block text-left text-gray-300 text-2xl pl-3 font-bold mb-5"
-            htmlFor="jobDescription"
-          >
-            Job Description
-          </label>
-          <textarea
-            id="jobDescription"
-            placeholder="Add Job Description"
-            className="w-full p-5 text-sm text-white bg-gray-800 border-0  focus:ring-0 rounded-2xl placeholder:text-[1rem] placeholder:font-medium resize-none"
-            rows={30}
-            cols={120}
-            value={jobDescription}
-            onChange={(e) => setJobDescription(e.target.value)}
-          ></textarea>
+      {loading && <div className="loader">Loading...</div>}
+      <h1 className="text-3xl font-bold mt-10 mb-5 tracking-wider">
+        Welcome to Resume Boost
+      </h1>
+      <p className="mb-5 font-medium tracking-wider">
+        Please upload your resume and the job description below
+      </p>
+      <form className="w-full max-w-7xl" onSubmit={handleBoostResume}>
+        <div className="flex justify-between gap-20 mb-8">
+          <div>
+            <label
+              className="block text-left text-gray-300 text-2xl pl-3 font-bold mb-5"
+              htmlFor="jobDescription"
+            >
+              Job Description
+            </label>
+            <textarea
+              id="jobDescription"
+              placeholder="Add Job Description"
+              className="w-full p-5 text-sm text-white bg-gray-800 border-0 focus:ring-0 rounded-2xl placeholder:text-[1rem] placeholder:font-medium resize-none"
+              rows={30}
+              cols={120}
+              value={jobDescription}
+              onChange={(e) => setJobDescription(e.target.value)}
+            ></textarea>
+          </div>
+          <div>
+            <label
+              className="block text-left text-gray-300 text-2xl pl-3 font-bold mb-5"
+              htmlFor="resume"
+            >
+              Resume
+            </label>
+            <textarea
+              id="resume"
+              className="w-full p-5 text-sm text-white bg-gray-800 border-0 focus:ring-0 rounded-2xl placeholder:text-[1rem] placeholder:font-medium resize-none"
+              rows={30}
+              placeholder="Mock Resume Data...."
+              cols={120}
+              value={resume}
+              onChange={(e) => setResume(e.target.value)}
+            ></textarea>
+          </div>
         </div>
-        <div className="">
-          <label
-            className="block text-left text-gray-300 text-2xl pl-3 font-bold mb-5"
-            htmlFor="resume"
+        {errorMessage && (
+          <div className="text-red-500 mb-5">{errorMessage}</div>
+        )}
+        <div className="flex items-center justify-between">
+          <button
+            type="submit"
+            onClick={handleBoostResume}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            disabled={loading}
           >
-            Resume
-          </label>
-          <textarea
-            id="resume"
-            className="w-full p-5 text-sm text-white bg-gray-800 border-0  focus:ring-0 rounded-2xl placeholder:text-[1rem] placeholder:font-medium resize-none"
-            rows={30}
-            placeholder="Mock Resume Data...."
-            cols={120}
-            value={resume}
-            onChange={(e) => setResume(e.target.value)}
-          ></textarea>
+            Boost Resume
+          </button>
+          <button
+            type="button"
+            onClick={handleGenerateCoverLetter}
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            disabled={loading}
+          >
+            Generate Cover Letter
+          </button>
         </div>
-      </div>
-      <div className="flex items-center justify-between">
-        <button
-          type="submit"
-          onClick={handleBoostResume}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        >
-          Boost Resume
-        </button>
-        <button
-          type="button"
-          onClick={handleGenerateCoverLetter}
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        >
-          Generate Cover Letter
-        </button>
-      </div>
-    </form>
-  </div>
+      </form>
+    </div>
   );
 };
 
